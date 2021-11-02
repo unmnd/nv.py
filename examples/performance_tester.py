182268a89b3e65d1c20081300143a1601d4916e5
Callum <callum.morrison@mac.com>
2021-11-02 16:49:04 +0000

    Add basic performance testing

--- examples/performance_tester.py
+++ examples/performance_tester.py
@@ -1,4 +1,6 @@
+import pickle
 import time
+from pathlib import Path
 
 from nv.node import Node
 
@@ -7,20 +9,45 @@ class PerformanceTester(Node):
     def __init__(self):
         super().__init__("performance_tester_node")
 
+        # Load the test_data as bytes
+        test_data_10mb = open(Path(__file__).parent / "10mb_data.pickle", "rb").read()
+        test_data_1mb = open(Path(__file__).parent / "1mb_data.pickle", "rb").read()
+        test_data_small = "this is a small text message"
+
+        self.test_data = [test_data_small, test_data_1mb, test_data_10mb]
+
         # Set up the subscriber
         self.subscriber = self.create_subscription(
             "performance_test_topic",
             self.subscriber_callback,
         )
 
-        # Start the node by saving the current time and immediately publishing
-        # to a topic
-        self.start_time = time.time()
-        self.publish("performance_test_topic", "This data doesn't matter")
+        self.start_times = [0] * 3
+
+        # For each data size, send a message and wait for a response
+        for i, data in enumerate(self.test_data):
+            self.start_times[i] = time.time()
+            self.log.info("Publishing test data...")
+            self.publish("performance_test_topic", data)
+            time.sleep(1)
 
     def subscriber_callback(self, msg):
+        finish_time = time.time()
+
+        self.log.info("Got message")
+
+        # Get index of test data
+        index = self.test_data.index(msg)
+
+        if index == 0:
+            self.log.info("Small message:")
+        elif index == 1:
+            self.log.info("1MB message:")
+        else:
+            self.log.info("10MB message:")
+
         self.log.info(
-            f"Time taken to send & receive message: {(time.time() - self.start_time) * 1000:.2}ms"
+            f"Time taken to send & receive message: {(finish_time - self.start_times[index]) * 1000:.5}ms"
         )
 
 
