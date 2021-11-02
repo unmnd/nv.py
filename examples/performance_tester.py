43e65df886990a680f93511f67059fc8a80f5725
Callum <callum.morrison@mac.com>
2021-11-02 17:35:36 +0000

    Expand performance tester for different datatypes

--- examples/performance_tester.py
+++ examples/performance_tester.py
@@ -1,20 +1,42 @@
-import pickle
+import os
 import time
 from pathlib import Path
 
 from nv.node import Node
 
 
+test_data = {
+    "Small String": "This is a small text message",
+    "Medium String": "This is a medium text message. It contains more characters than the previous small string, but in the grand scheme of data transfer it's still pretty small. Following this message is a short poem to further pad this data. According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello?",
+    "Float": 1.23456789,
+    "Integer": 123456789,
+    "Dictionary": {
+        "key1": "value1",
+        "key2": "value2",
+        "key3": 11223344,
+        "key4": True,
+        "key5": False,
+    },
+    "List": [
+        "value1",
+        "value2",
+        "value3",
+        "value4",
+        "value5",
+        "value6",
+        "value7",
+        "value8",
+        "value9",
+        "value10",
+    ],
+    "1MB of random bytes": os.urandom(1024 * 1024),
+    "10MB of random bytes": os.urandom(10 * 1024 * 1024),
+}
+
+
 class PerformanceTester(Node):
     def __init__(self):
-        super().__init__("performance_tester_node")
-
-        # Load the test_data as bytes
-        test_data_10mb = open(Path(__file__).parent / "10mb_data.pickle", "rb").read()
-        test_data_1mb = open(Path(__file__).parent / "1mb_data.pickle", "rb").read()
-        test_data_small = "this is a small text message"
-
-        self.test_data = [test_data_small, test_data_1mb, test_data_10mb]
+        super().__init__("performance_tester_node", skip_registration=True)
 
         # Set up the subscriber
         self.subscriber = self.create_subscription(
@@ -22,33 +44,37 @@ class PerformanceTester(Node):
             self.subscriber_callback,
         )
 
-        self.start_times = [0] * 3
+        self.start_times = {}
+        self.durations = {}
+
+        self.waiting_on_message = False
 
         # For each data size, send a message and wait for a response
-        for i, data in enumerate(self.test_data):
-            self.start_times[i] = time.time()
-            self.log.info("Publishing test data...")
-            self.publish("performance_test_topic", data)
-            time.sleep(1)
+        for key, value in test_data.items():
+            self.start_times[key] = time.perf_counter()
+            self.log.debug("Publishing test data...")
+
+            self.waiting_on_message = True
+            self.publish("performance_test_topic", value)
+
+            while self.waiting_on_message:
+                time.sleep(0.1)
+
+        self.log.info("\n\n---\n\nDurations:")
+        for key, value in self.durations.items():
+            self.log.info(f"{key}: {value:.5}ms")
 
     def subscriber_callback(self, msg):
-        finish_time = time.time()
+        finish_time = time.perf_counter()
 
-        self.log.info("Got message")
+        self.log.debug("Got message")
 
-        # Get index of test data
-        index = self.test_data.index(msg)
+        # Get key of test data
+        data_key = [key for key, value in test_data.items() if value == msg][0]
 
-        if index == 0:
-            self.log.info("Small message:")
-        elif index == 1:
-            self.log.info("1MB message:")
-        else:
-            self.log.info("10MB message:")
+        self.durations[data_key] = (finish_time - self.start_times[data_key]) * 1000
 
-        self.log.info(
-            f"Time taken to send & receive message: {(finish_time - self.start_times[index]) * 1000:.5}ms"
-        )
+        self.waiting_on_message = False
 
 
 def main():
