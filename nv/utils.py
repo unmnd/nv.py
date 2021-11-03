e7278a41a249a2e87fe1a6d79315cbdf8d0db435
Callum <callum.morrison@mac.com>
2021-11-03 15:46:11 +0000

    Remove old functionality

--- nv/utils.py
+++ nv/utils.py
@@ -21,33 +21,3 @@ CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".nv")
 # Generate the config path if it doesn't exist
 if not os.path.exists(CONFIG_PATH):
     os.makedirs(CONFIG_PATH)
-
-
-def node_exists(host, node_id=None, node_name=None, node_ip=None):
-    """
-    Check if a node exists in the network. If the node exists, information
-    about the node is returned, otherwise None.
-    """
-
-    # Ensure only one node identifier is provided
-    assert (
-        bool(node_id) ^ bool(node_name) ^ bool(node_ip)
-    ), "You must specify exactly one of `node_id`, `node_name`, or `node_ip`."
-
-    params = {
-        "node_id": node_id,
-        "node_name": node_name,
-        "node_ip": node_ip,
-    }
-
-    r = requests.get(host + "/api/get_node_info", params=params)
-
-    if r.status_code == 200:
-        if r.json().get("status") == "success":
-            return r.json().get("node")
-        else:
-            return None
-
-    raise Exception(
-        f"Unable to check if the node exists. Got status code: {r.status_code}"
-    )
